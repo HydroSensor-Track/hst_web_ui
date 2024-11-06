d0e98505a288db2b3e8c968c5afe0060bd81d943
Martin Stefanelli <martin.stefanelli.96@hotmail.com>
2024-11-06 00:32:12 -0300

    Add useEffect for render

--- src/containers/HstWebUi.tsx
+++ src/containers/HstWebUi.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const HstWebUi = () => {
     const navigate = useNavigate();
 
-    navigate("/");
+    useEffect(() => {
+        console.log("HstWebUi");
+        navigate("/");
+    }, [])
 
     return null;
 };
